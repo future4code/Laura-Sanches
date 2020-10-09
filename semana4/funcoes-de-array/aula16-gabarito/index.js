e0a3d87db6d7f449488df11a1e49e291e5c51226
laurasanches <lalinhabom@gmail.com>
2020-10-09 08:43:48 -0300

    semana4-aula16

--- semana4/funcoes-de-array/aula16-gabarito/index.js
+++ semana4/funcoes-de-array/aula16-gabarito/index.js
@@ -1,119 +1,113 @@
-let arrDespesas = []
-imprimirDespesas(arrDespesas)
-imprimirExtrato()
-
+let arrDespesas = [];
+imprimirDespesas(arrDespesas);
+imprimirExtrato();
 
 // PRIMEIRO
-function imprimirDespesas(despesas){
-    let divDespesas = document.getElementById('despesas')
-    divDespesas.innerHTML = '<p><u>Despesas Detalhadas</u></p>'
+function imprimirDespesas(despesas) {
+  let divDespesas = document.getElementById("despesas");
+  divDespesas.innerHTML = "<p><u>Despesas Detalhadas</u></p>";
 
-    despesas.forEach((despesa, index, despesas) => {
-        despesas = [{valor : number(document.getElementById('valorCadastro')),
-        tipo: (document.getElementById('tipoCadastro')),
-        descricao: (document.getElementById('descricaoCadastro'))}
-        ]
-
-    divDespesas.innerHTML += `<p>valor: R$ ${despesas.valor} | tipo: ${despesa.tipo} | descricao: ${despesa.descricao}</p>`
-
-    })
-
-    // AQUI VEM A IMPLEMENTAÇÃO
+  despesas.forEach((despesas) => {
+    divDespesas.innerHTML += `<p>valor: R$ ${despesas.valor}| tipo: ${despesas.tipo} | descrição: ${despesas.descricao}<p/>`;
+  });
 }
 
+// SEGUNDO
+function imprimirExtrato() {
+  let divExtrato = document.getElementById("extrato");
+  let gastoTotal = 0;
+  let gastoAlimentacao = 0;
+  let gastoUtilidades = 0;
+  let gastoViagem = 0;
 
-// SEGUNDO 
-function imprimirExtrato(){
-    let divExtrato = document.getElementById('extrato')
-    let gastoTotal = 0
-    let gastoAlimentacao = 0
-    let gastoUtilidades = 0
-    let gastoViagem = 0
+  arrDespesas.forEach((despesa) => {
+    if (despesa.tipo === "alimentação") {
+      gastoAlimentacao += despesa.valor;
+    } else if (despesa.tipo === "utilidades") {
+      gastoUtilidades += despesa.valor;
+    } else {
+      gastoViagem += despesa.valor;
+    }
 
+    gastoTotal = gastoUtilidades + gastoAlimentacao + gastoViagem;
+  });
 
-    // AQUI VEM A IMPLEMENTAÇÃO
-
-    divExtrato.innerHTML = `<p>Extrato: Gasto Total: R$${gastoTotal} | Alimentação: R$${gastoAlimentacao} | 
-                                        Utilidades: R$${gastoUtilidades} | Viagem: R$${gastoViagem}</p>`
+  divExtrato.innerHTML = `<p>Extrato: Gasto Total: R$${gastoTotal} | Alimentação: R$${gastoAlimentacao} | 
+                                        Utilidades: R$${gastoUtilidades} | Viagem: R$${gastoViagem}</p>`;
 }
-
 
 function limparFiltros() {
-    document.getElementById('tipoFiltro').value = ""
-    document.getElementById('valorFiltroMin').value = ""
-    document.getElementById('valorFiltroMax').value = ""
+  document.getElementById("tipoFiltro").value = "";
+  document.getElementById("valorFiltroMin").value = "";
+  document.getElementById("valorFiltroMax").value = "";
 }
 
+function adicionarDespesa() {
+  let valorCdt = document.getElementById("valorCadastro");
+  let tipoCtd = document.getElementById("tipoCadastro");
+  let descricaoCtd = document.getElementById("descricaoCadastro");
 
+  if (
+    validarValor(valorCdt) &&
+    validarTipo(tipoCtd) &&
+    validarDescricao(descricaoCtd)
+  ) {
+    let novaDespesa = {
+      valor: Number(valorCdt.value),
+      tipo: tipoCtd.value,
+      descricao: descricaoCtd.value,
+    };
 
-function adicionarDespesa(){
-    let valorCdt = document.getElementById('valorCadastro')
-    let tipoCtd = document.getElementById('tipoCadastro')
-    let descricaoCtd = document.getElementById('descricaoCadastro')
+    arrDespesas.push(novaDespesa);
 
-    if(validarValor(valorCdt) && validarTipo(tipoCtd) && validarDescricao(descricaoCtd)){
-        let novaDespesa = {
-            valor: Number(valorCdt.value),
-            tipo: tipoCtd.value,
-            descricao: descricaoCtd.value,
-        }
+    valorCdt.value = "";
+    tipoCtd.value = "";
+    descricaoCtd.value = "";
 
-        arrDespesas.push(novaDespesa)
-        
-        valorCdt.value = ""
-        tipoCtd.value = ""
-        descricaoCtd.value = ""
-
-        
-        limparFiltros()
-        imprimirDespesas(arrDespesas)
-        imprimirExtrato()
-    } else {
-        alert("`Faltou algum valor ou algum valor é um número negativo`")
-    }
+    limparFiltros();
+    imprimirDespesas(arrDespesas);
+    imprimirExtrato();
+  } else {
+    alert("`Faltou algum valor ou algum valor é um número negativo`");
+  }
 }
-
-
 
 // TERCEIRO
-function filtrarDespesas(){
-    let tipoFiltro = document.getElementById('tipoFiltro').value
-    let valorMin = Number(document.getElementById('valorFiltroMin').value)
-    let valorMax = Number(document.getElementById('valorFiltroMax').value)
+function filtrarDespesas() {
+  let tipoFiltro = document.getElementById("tipoFiltro").value;
+  let valorMin = Number(document.getElementById("valorFiltroMin").value);
+  let valorMax = Number(document.getElementById("valorFiltroMax").value);
 
+  let despesasFiltradas = arrDespesas.filter((despesa) => {
+    if (
+      (tipoFiltro === despesa.tipo || tipoFiltro === "todos") &&
+      despesa.valor >= valorMin &&
+      despesa.valor <= valorMax
+    ) {
+      return true;
+    }
+  });
 
-    let despesasFiltradas // AQUI NESSA VARIÁVEL VEM A IMPLEMENTAÇÃO
-
-    imprimirDespesas(despesasFiltradas)
+  imprimirDespesas(despesasFiltradas);
 }
 
-
-
-
-
-
-
-// FunÇoes que fazem validaÇoes dos inputs de criaÇao de despesas 
-
-// NÃO SE PREOCUPEM EM ENTENDER ESSAS FUNÇÕES
-
-function validarValor(valor){
-    if(valor.value.length > 0 && parseInt(valor.value) > 0){
-        return true
-    }
-    return false
+function validarValor(valor) {
+  if (valor.value.length > 0 && parseInt(valor.value) > 0) {
+    return true;
+  }
+  return false;
 }
 
-function validarTipo(tipo){
-    if(tipo.value !== ""){
-        return true
-    }
-    return false
+function validarTipo(tipo) {
+  if (tipo.value !== "") {
+    return true;
+  }
+  return false;
 }
 
-function validarDescricao(texto){
-    if(texto.value.replace(/ /g,"").length !== 0){
-        return true
-    }
-    return false
+function validarDescricao(texto) {
+  if (texto.value.replace(/ /g, "").length !== 0) {
+    return true;
+  }
+  return false;
 }
