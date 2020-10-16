5d4f4c29c4126a170aaa7acae9d180ae6fb6e328
laurasanches <71328526+laurasanches@users.noreply.github.com>
2020-10-16 16:14:46 -0300

    semana5-aula20

---
+++ semana5/insta4-gabarito/src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import './App.css';
+import Post from './components/Post/Post';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className={'app-container'}>
+        <Post
+          nomeUsuario={'paulinha'}
+          fotoUsuario={'https://picsum.photos/50/50'}
+          fotoPost={'https://picsum.photos/200/150'}
+        />
+        <Post
+          nomeUsuario={'joao'}
+          fotoUsuario={'https://picsum.photos/50/51'}
+          fotoPost={'https://picsum.photos/200/151'}
+        />
+        <Post
+          nomeUsuario={'amanda'}
+          fotoUsuario={'https://picsum.photos/50/52'}
+          fotoPost={'https://picsum.photos/200/152'}
+        />
+      </div>
+    );
+  }
+}
+
+export default App;
