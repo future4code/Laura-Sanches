014da7a1eb51681ce5cadcb89c3459a395127975
laurasanches <lalinhabom@gmail.com>
2020-11-17 14:49:16 -0300

    labeX-projeto

--- semana8/labex-projeto/src/App.js
+++ semana8/labex-projeto/src/App.js
@@ -1,11 +1,12 @@
 import React, {useEffect, useState} from 'react';
 import axios from 'axios';
 import styled from 'styled-components';
+import Router from './components/HomePage/Router/Router';
 
 function App() {
   return (
     <div>
-        App
+        <Router/>
     </div>
   );
 }
--- semana8/labex-projeto/src/components/HomePage/Router/Router.js
+++ semana8/labex-projeto/src/components/HomePage/Router/Router.js
@@ -1,12 +1,38 @@
 import React, {useState, useEffect} from 'react';
 import styled from 'styled-components';
-import axios from axios;
+import axios from 'axios';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import HomePage from '../HomePage';
+import LoginPage from '../LoginPage/LoginPage';
+import TripDetailsPage from '../TripDetailsPage/TripeDetailsPage';
+import ListTripsPage from '../ListTripsPage/ListTripsPage';
+import ApplicationFormPage from '../ApplicationFormPage/ApplicationFormPage';
+import CreateTripPage from '../CreateTripPage/CreateTripPage';
 
 function Router() {
     return(
-        <div>
-            Router → Para administrar todas as rotas
-        </div>
+        <BrowserRouter>
+            <Switch>
+                <Route exact path='/'>
+                    <HomePage/>
+                </Route>
+                <Route exact path='/login'>
+                    <LoginPage/>
+                </Route>
+                <Route exact path='/details'>
+                    <TripDetailsPage/>
+                </Route>
+                <Route exact path='/trips-page'>
+                    <ListTripsPage/>
+                </Route>
+                <Route exact path='/form-page'>
+                    <ApplicationFormPage/>
+                </Route>
+                <Route exact path='/create-trip'>
+                    <CreateTripPage/>
+                </Route>
+            </Switch>
+        </BrowserRouter>
     )
 }
 
