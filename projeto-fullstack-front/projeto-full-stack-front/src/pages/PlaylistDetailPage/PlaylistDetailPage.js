ff24fcf7b7b2acd9bd401a1deeb386426e41d99a
laurasanches <lalinhabom@gmail.com>
2021-03-19 09:36:51 -0300

    algumas configs na página de detalhes

--- projeto-fullstack-front/projeto-full-stack-front/src/pages/PlaylistDetailPage/PlaylistDetailPage.js
+++ projeto-fullstack-front/projeto-full-stack-front/src/pages/PlaylistDetailPage/PlaylistDetailPage.js
@@ -1,14 +1,22 @@
+import { Typography } from '@material-ui/core';
 import React from 'react';
+import { useParams } from 'react-router-dom';
 import useProtectedPage from '../../hooks/useProtectedPage';
+import useRequestData from '../../hooks/useRequestData';
+import {PlaylistContainer, ScreenContainer} from './styled'
 
 const PlaylistDetailPage = () => {
+    const params = useParams()
+    const playlist = useRequestData([], `https://backend-fullstack-labenu.herokuapp.com/music/${params.id}`) [0]
+
     useProtectedPage()
     return(
-        <div>
-            <h1>
-                PlaylistDetailPage
-            </h1>
-        </div>
+        <ScreenContainer>
+            <PlaylistContainer>
+                <Typography gutterBottom align={'center'} variant={'h5'} color={'primary'}>{playlist.title}</Typography>
+                <Typography align={'center'}>{playlist.author}</Typography>
+            </PlaylistContainer>
+        </ScreenContainer>
     )
 }
 
--- projeto-fullstack-front/projeto-full-stack-front/src/pages/PlaylistListPage/PlaylistListPage.js
+++ projeto-fullstack-front/projeto-full-stack-front/src/pages/PlaylistListPage/PlaylistListPage.js
@@ -4,15 +4,16 @@ import {PlaylistListContainer, AddPlaylistButton} from './styled';
 import PlaylistCard from '../../components/PlaylistCard/PlaylistCard'
 import useRequestData from '../../hooks/useRequestData';
 import {Add} from '@material-ui/icons';
-import { goToAddPlaylist } from '../../routes/coordinator';
+import { goToAddPlaylist, goToDetailPlaylist } from '../../routes/coordinator';
 import { useHistory } from 'react-router-dom';
 
 const PlaylistListPage = () => {
     useProtectedPage()
     const history = useHistory()
     const playlists = useRequestData([], 'https://backend-fullstack-labenu.herokuapp.com/user/feed')
-    const onClickCard = () => {
 
+    const onClickCard = (id) => {
+      goToDetailPlaylist(history, id)
     }
 
     const playlistCards = playlists && playlists.feed && playlists.feed.result && playlists.feed.result.musics.map((playlist) => {
@@ -23,7 +24,7 @@ const PlaylistListPage = () => {
               file={playlist.file}
               author={playlist.author}
               date={playlist.date}
-              onClick={onClickCard}
+              onClick={() => onClickCard(playlist.playlist_id)}
             />
           )
     })
